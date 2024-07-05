c4a78ff9123dd778b5ae504b7ff3ad67138adb7a
manish <mmanish9090@gmail.com>
2024-07-05 14:30:31 +0530

    added the arraylist for city

---
+++ ArrayList/src/com/velocity/Arraylist/ArrayListDemo.java
@@ -0,0 +1,16 @@
+package com.velocity.Arraylist;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListDemo {
+
+	public static void main(String[] args) {
+
+		List<String> list = new ArrayList<String>();
+		list.add("pune");
+		list.add("mumbai");
+		list.add("thane");
+		System.out.println(list);
+	}
+}
